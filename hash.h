1387c83934c3f1c39b316a5e7460ada32fe2140a
shriram98 <shriramv@hotmail.com>
2017-09-15 12:18:38 +0530

    Add files via upload

---
+++ hash.h
@@ -0,0 +1,7 @@
+void init(int *,int);
+int hash(int);
+void insert(int,int*,char);
+int probe(char,int,int,int*,char);
+//int lin_probe(char,int,int,int*,int);
+void display(int*);
+int search(int,int*,char);
